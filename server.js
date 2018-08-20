f0576851f0120a4f20fb7628ae92a65d920ad95b
Kai-Lin <neversaynever0502@gmail.com>
2018-08-20 17:19:21 +0800

    fux

---
+++ server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const express = require('express');
+var Sequelize = require('sequelize');
+
+
+const Op = Sequelize.Op;
+var sequelize = new Sequelize('postgres', 'postgres', 'password', {
+  host : 'db', 
+  port : '5432', 
+  dialect : 'postgres',
+  operatorsAliases: Op,
+});
+
+sequelize
+.authenticate()
+.then(() => {
+  console.log('Connection has been established successfully.');
+})
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
+
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  // res.send('Hello world\n');
+  res.json({text:'hello'})
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}=>NODE_ENV:${process.env.NODE_ENV}`);
